5d6069f661c296cabdb6d28dbe4dc1933c8a9ad5
AstraStarDeveloper <claryanebispo@gmail.com>
2026-07-24 23:32:29 -0300

    Change raised amount in campaign configuration
    Updated the amount raised for the campaign from 900 to 655.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Configuração da Vaquinha para a OBAFOG
 // Altere aqui o valor arrecadado quando entrar mais PIX!
 const meta = 10000;
-let arrecadado = 900;
+let arrecadado = 655;
 
 function atualizarVaquinha(){
 
