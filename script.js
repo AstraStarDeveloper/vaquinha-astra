46a5f9200bf37a58a8da39632ad5ff82354a29dc
AstraStarDeveloper <claryanebispo@gmail.com>
2026-07-25 22:51:09 -0300

    Update fundraising amount and progress bar
    Updated the collected amount in the fundraising configuration and ensured the progress bar reflects the new value.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Configuração da Vaquinha para a OBAFOG
 // Altere aqui o valor arrecadado quando entrar mais PIX!
 const meta = 10000;
-let arrecadado = 655;
+let arrecadado = 675;
 
 function atualizarVaquinha(){
 
