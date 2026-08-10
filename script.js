492b33a69efc009b05639ef33a4c85e39db226ba
AstraStarDeveloper <claryanebispo@gmail.com>
2026-08-10 20:52:24 -0300

    Update fundraising amount and improve code readability
    Updated the amount raised in the fundraising campaign and ensured the progress bar reflects the new value. Adjusted the code for better readability and consistency.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Configuração da Vaquinha para a OBAFOG
 // Altere aqui o valor arrecadado quando entrar mais PIX!
 const meta = 10000;
-let arrecadado = 1145;
+let arrecadado = 1425;
 
 function atualizarVaquinha(){
 
