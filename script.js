e9e7f87fc98e03731cb79bd56ddb06a3e7457b72
AstraStarDeveloper <claryanebispo@gmail.com>
2026-08-17 13:32:55 -0300

    Set total amount raised to 10000
    Updated the collected amount in the fundraising configuration to reflect the total amount raised.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 // Configuração da Vaquinha para a OBAFOG
 // Altere aqui o valor arrecadado quando entrar mais PIX!
 const meta = 10000;
-let arrecadado = 1425;
+let arrecadado = 10000;
 
 function atualizarVaquinha(){
 
